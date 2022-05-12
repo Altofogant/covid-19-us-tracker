97d0626fe42a2eb6f1c93f67c92f4c5ef020fe68
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-05-12 15:47:16 +0300

    fixed bulma error and got navbar working

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,3 +1,34 @@
 <script>
-	//export let segment;
+	let isOpen = false;
+	export let segment;
+	function toggleNav() {
+		isOpen = !isOpen;
+	}
 </script>
+
+<style>
+	@media(min-width: 769px) {
+		nav {
+			border-bottom: solid gray 1px;
+		}
+	}
+</style>
+
+<nav class="navbar" aria-label="main navigation">
+	<div class="navbar-brand">
+		<a href="/" class="navbar-item">
+			<img src="logo.jpg" alt="virus" />
+		</a>
+		<span class="navbar-burger" on:click={toggleNav} class:is-active={isOpen} aria-label="menu" aira-expanded="false" >
+			<span aria-hidden="true"></span>
+			<span aria-hidden="true"></span>
+			<span aria-hidden="true"></span>
+		</span>
+	</div>
+	<div class="navbar-menu" class:is-active={isOpen}>
+		<div class="navbar-start">
+			<a href="/" class="navbar-item" class:is-active={segment === undefined}>Home</a>
+			<a href="/about" class="navbar-item"  class:is-active={segment === 'about'}>About</a>
+		</div>
+	</div>
+</nav>
