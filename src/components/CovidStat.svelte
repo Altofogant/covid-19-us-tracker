9cd97452f379a759c2e1e6155be50806b7035082
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-05-12 18:13:50 +0300

    added 404 and other error catch and messages

--- src/components/CovidStat.svelte
+++ src/components/CovidStat.svelte
@@ -11,10 +11,10 @@
 <div class="section">
     <div class="container">
         <div class="columns">
-            <h2 class="is-3 column">Cases: {usStats['cases']}</h2>
-            <h2 class="is-3 column">Deaths: {usStats['deaths']}</h2>
-            <h2 class="is-3 column">Recovered: {usStats['recovered']}</h2>
-            <h2 class="is-3 column">Updated: {usStats['updated']}</h2>
+            <h2 class="is-3 column">Cases: {usStats.cases}</h2>
+            <h2 class="is-3 column">Deaths: {usStats.deaths}</h2>
+            <h2 class="is-3 column">Recovered: {usStats.recovered}</h2>
+            <h2 class="is-3 column">Updated: {usStats.updated}</h2>
         </div>
     </div>
 </div>
@@ -22,10 +22,10 @@
 <div class="section">
     <div class="container">
         <div class="columns">
-            <h2 class="is-3 column">Tested: {usStats['tested']}</h2>
-            <h2 class="is-3 column">Hospitalized: {usStats['hospitalized']}</h2>
-            <h2 class="is-3 column">Total ICU: {usStats['icu']}</h2>
-            <h2 class="is-3 column">Total Ventilator: {usStats['ventilator']}</h2>
+            <h2 class="is-3 column">Tested: {usStats.tested}</h2>
+            <h2 class="is-3 column">Hospitalized: {usStats.hospitalized}</h2>
+            <h2 class="is-3 column">Total ICU: {usStats.icu}</h2>
+            <h2 class="is-3 column">Total Ventilator: {usStats.ventilator}</h2>
         </div>
     </div>
 </div>
