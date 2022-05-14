9f314733b1111b274c26f1a539f514b6d5e849bc
Altofogant <105163865+Altofogant@users.noreply.github.com>
2022-05-14 23:58:42 +0300

    added prefetch to all links

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -16,7 +16,7 @@
 
 <nav class="navbar" aria-label="main navigation">
 	<div class="navbar-brand">
-		<a href="/" class="navbar-item">
+		<a rel="prefetch" href="/" class="navbar-item">
 			<img src="logo.jpg" alt="virus" />
 		</a>
 		<span class="navbar-burger" on:click={toggleNav} class:is-active={isOpen} aria-label="menu" aira-expanded="false" >
@@ -27,8 +27,8 @@
 	</div>
 	<div class="navbar-menu" class:is-active={isOpen}>
 		<div class="navbar-start">
-			<a href="/" class="navbar-item" class:is-active={segment === undefined}>Home</a>
-			<a href="/about" class="navbar-item"  class:is-active={segment === 'about'}>About</a>
+			<a rel="prefetch" href="/" class="navbar-item" class:is-active={segment === undefined}>Home</a>
+			<a rel="prefetch" href="/about" class="navbar-item"  class:is-active={segment === 'about'}>About</a>
 		</div>
 	</div>
 </nav>
